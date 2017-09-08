9828e73921b82f95661059d245ea619b7cf8acac
mpleier <mpleier@gmail.com>
2017-09-08 13:57:18 -0700

    words

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres://otfbzppwqgautv:92788e3d4a3f056c3674ddd8299b6e7a7dbda21a8645777c21623ac556290d71@ec2-184-72-248-8.compute-1.amazonaws.com:5432/ddas5d1bba36av');
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+module.exports = sequelize;
