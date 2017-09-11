4a8d59c912c32e717e7ae7f312aafff9d62f47d9
mpleier <mpleier@gmail.com>
2017-09-11 14:20:01 -0700

    Break time.

--- controllers/concertController.js
+++ controllers/concertController.js
@@ -4,15 +4,15 @@ var router = express.Router();
 
 app.set('models', require('../models/reviews.js'));
 var review = app.get('models');
-console.log(review);
+// console.log(review);
 
 
 // Create all our routes and set up logic within those routes where required.
 router.get('/', function(req, res) {
-  console.log(review);
+  // console.log(review);
   review.findAll({raw: true}).then(x => {
-  console.log(x);
-  res.render('index', {x});
+  // console.log(x);
+res.render('index', {x});
 });
 });
 
@@ -30,13 +30,36 @@ router.post("/:Artist", function(req, res) {
   res.render('index', {x});
   });
 
-
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function() {
-    res.redirect("/");
-  });
 });
+
+
+router.post("/api/:Artist:Venue:Review", function(req, res) {
+  var a = req.body.Artist;
+  var v = req.body.Venue;
+  var r = req.body.Review;
+  console.log(a);
+  console.log(v);
+  console.log(r);
+
+
+  review
+    .build({
+        Artist: 'Arnold',
+        Venue: 'Palmer',
+        Review:"Golf."})
+    .save()
+    .catch(error => {
+      // Ooops, do some error-handling
+    });
+res.redirect("/");
+
+
+
+
+
+});
+
+
 
 router.delete("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
