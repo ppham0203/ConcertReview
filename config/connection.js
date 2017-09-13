296b7b2b5a49ea5aaa22946c60bfd8997b2a7001
mpleier <mpleier@gmail.com>
2017-09-13 11:08:09 -0700

    Hello

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize('postgres://zokizcuocorcfk:b11e184bfbfcc802004e99d38121b63941f30270b91da6fd9046f5c0159826ae@ec2-54-225-88-199.compute-1.amazonaws.com:5432/d5u3c0qj2tom3r');
-const sequelize = new Sequelize('postgres', 'mpleier@gmail.com', null, {
-  host: 'localhost',
-  dialect: 'postgres',
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-
-});
+const sequelize = new Sequelize('postgres://zokizcuocorcfk:b11e184bfbfcc802004e99d38121b63941f30270b91da6fd9046f5c0159826ae@ec2-54-225-88-199.compute-1.amazonaws.com:5432/d5u3c0qj2tom3r');
+// const sequelize = new Sequelize('postgres', 'mpleier@gmail.com', null, {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   },
+//
+// });
 
 sequelize
   .authenticate()
